8bdbb4825dc6f0e8e71a27846f2da48e2f563b0b
SirDraiger <lekht-k@yandex.ru>
2024-03-30 12:08:38 +0300

    basket button validate

--- src/components/common/Basket.ts
+++ src/components/common/Basket.ts
@@ -10,6 +10,7 @@ interface IBasketView {
 export class Basket extends Component<IBasketView> {
   protected _list: HTMLElement;
   protected _total: HTMLElement;
+  protected _button: HTMLButtonElement;
 
 
   constructor(container: HTMLElement, protected events: EventEmitter) {
@@ -17,6 +18,7 @@ export class Basket extends Component<IBasketView> {
 
     this._list = ensureElement<HTMLElement>('.basket__list', this.container);
     this._total = container.querySelector('.basket__price');
+    this._button = container.querySelector('.basket__button');
 
     this.items = [];
   }
@@ -33,5 +35,11 @@ export class Basket extends Component<IBasketView> {
 
   set total(value: number) {
     this.setText(this._total, `${value} синапсов`);
+    
+    if(value === 0) {
+      this.setDisabled(this._button, true);
+    } else {
+      this.setDisabled(this._button, false);
+    }
   }
 }
